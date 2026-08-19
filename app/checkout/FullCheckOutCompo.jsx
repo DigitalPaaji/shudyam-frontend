52055209360e8b491b66e35a959e0defb0a09a1f
jonty1231 <jontythakur68@gmail.com>
2026-08-19 12:50:33 +0530

    ASd

--- app/checkout/FullCheckOutCompo.jsx
+++ app/checkout/FullCheckOutCompo.jsx
@@ -227,10 +227,10 @@ const handleSubmitPayment = async () => {
 
 
         
-        <div className='  px-4 md:px-12 lg:px-24 xl:px-40 py-24 grid lg:grid-cols-3 gap-5'>
-<div className=' md:col-span-2'>
+        <div className='  px-4 md:px-12 lg:px-24 xl:px-40 py-24 grid  lg:grid-cols-3 gap-5'>
+<div className=' lg:col-span-2'>
 
-{/* <AddressCompo  selectedAddressId={checkoutData.address} setSelectedAddressId={(itm)=>setCheckoutData(prev=>({...prev,address:itm}))}    /> */}
+<AddressCompo  selectedAddressId={checkoutData.address} setSelectedAddressId={(itm)=>setCheckoutData(prev=>({...prev,address:itm}))}    />
 
 
 
@@ -238,9 +238,9 @@ const handleSubmitPayment = async () => {
     </div>
 
 
-    <div className=' md:col-span-1 w-full'>
+    {/* <div className=' lg:col-span-1 '>
       <CheckoutProduct handelSubmitPayment={handleSubmitPayment} product={cartParam} checkoutData={checkoutData}   setCheckoutData={(item)=>setCheckoutData(prev=>({...prev,...item}))}/>
-    </div>
+    </div> */}
 
 
 
